07d200248466d475cbee3ca82e56fbe6467a7094
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-10-05 22:43:12 +0000

    event toggle working

--- sandbox.js
+++ sandbox.js
@@ -11,30 +11,28 @@ let board = [ , , , , , , , , ];
 let winConditionals = []
 
 function xTurn() {
-    Element.innerText = initState.x
+    //  = initState.x
     initState.turn = 'o';
     initState.turnCount++
 };
 
 function oTurn() {
-    Element.innerText = initState.o
+    //  = initState.o
     initState.turn = 'x'
     initState.turnCount++
 
 }
 
-xTurn()
-oTurn()
-xTurn()
+
 
 function checkTurn() {
     if (initState.turn === 'x') {
         console.log('player x turn')
-        // xTurn()
+        xTurn()
         
     } else if (initState.turn === 'o') {
         console.log('player o turn')
-        // oTurn()
+        oTurn()
     };
 };
 checkTurn()
@@ -55,45 +53,87 @@ function createLayout(parentEl, tag, text, className, idName,) {
 
 function renderPage(){
     createLayout(mainDiv, 'div', '', 'row', 'topRow')
-    createLayout(topRow, 'div', 'x', 'col', 'col1')
-    createLayout(topRow, 'div', 'x', 'col', 'col2')
-    createLayout(topRow, 'div', 'x', 'col', 'col3')
+    createLayout(topRow, 'button', '', 'col btn', 'col1')
+    createLayout(topRow, 'button', '', 'col btn', 'col2')
+    createLayout(topRow, 'button', '', 'col btn', 'col3')
     createLayout(mainDiv, 'div', '', 'row', 'middleRow')
-    createLayout(middleRow, 'div', 'x', 'col', 'col4')
-    createLayout(middleRow, 'div', 'x', 'col', 'col5')
-    createLayout(middleRow, 'div', 'x', 'col', 'col6')
+    createLayout(middleRow, 'button', '', 'col btn', 'col4')
+    createLayout(middleRow, 'button', '', 'col btn', 'col5')
+    createLayout(middleRow, 'button', '', 'col btn', 'col6')
     createLayout(mainDiv, 'div', '', 'row', 'bottomRow')
-    createLayout(bottomRow, 'div', 'x', 'col', 'col7')
-    createLayout(bottomRow, 'div', 'x', 'col', 'col8')
-    createLayout(bottomRow, 'div', 'x', 'col', 'col9')
+    createLayout(bottomRow, 'button', '', 'col btn', 'col7')
+    createLayout(bottomRow, 'button', '', 'col btn', 'col8')
+    createLayout(bottomRow, 'button', '', 'col btn', 'col9')
 
 }
 renderPage()
 
 col1.addEventListener('click', () =>{
     console.log('clicked')
+    col1.innerText = `${initState.turn}`
+    checkTurn()
+    col1.setAttribute('disabled', '')
 })
+
 col2.addEventListener('click', () =>{
     console.log('clicked')
+    col2.innerText = `${initState.turn}`
+        checkTurn()
+        col2.setAttribute('disabled', '')
+
 })
+
 col3.addEventListener('click', () =>{
     console.log('clicked')
+    col3.innerText = `${initState.turn}`
+        checkTurn()
+        col3.setAttribute('disabled', '')
+
 })
+
 col4.addEventListener('click', () =>{
     console.log('clicked')
+    col4.innerText = `${initState.turn}`
+        checkTurn()
+        col4.setAttribute('disabled', '')
+
 })
+
 col5.addEventListener('click', () =>{
     console.log('clicked')
+    col5.innerText = `${initState.turn}`
+        checkTurn()
+        col5.setAttribute('disabled', '')
+
 })
+
 col6.addEventListener('click', () =>{
     console.log('clicked')
+    col6.innerText = `${initState.turn}`
+        checkTurn()
+        col6.setAttribute('disabled', '')
+
 })
 col7.addEventListener('click', () =>{
     console.log('clicked')
+    col7.innerText = `${initState.turn}`
+        checkTurn()
+        col7.setAttribute('disabled', '')
+
 })
+
 col8.addEventListener('click', () =>{
     console.log('clicked')
+    col8.innerText = `${initState.turn}`
+        checkTurn()
+        col8.setAttribute('disabled', '')
+
 })
+
 col9.addEventListener('click', () =>{
     console.log('clicked')
+    col9.innerText = `${initState.turn}`
+        checkTurn()
+        col9.setAttribute('disabled', '')
+
 })
