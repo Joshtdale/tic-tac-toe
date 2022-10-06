ff103e69136d7a2b39a9add6868414b3fee3fbe4
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-10-06 01:34:06 +0000

    check winner conditional and style

--- sandbox.js
+++ sandbox.js
@@ -2,26 +2,37 @@ let initState = {
     turn: 'o',
     winner: '',
     turnCount: 0,
-    image: './images/skull.png',
+    image: './images/crossbones.png',
+    imageSize: '100px',
     x: 'x',
     o: '0'
     
 };
 // console.log(initState.turn)
 let board = [ , , , , , , , , ];
-let winConditionals = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+let winConditionals = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,5,7],[2,5,8],[0,4,8],[2,4,6]]
 // console.log(winConditionals)
 
 function xTurn() {
     //  = initState.x
     initState.turn = 'o';
+    initState.image = './images/skull.png'
+    initState.imageSize = '50px'
     initState.turnCount++
+    if (initState.turnCount > 5){
+        console.log('possible winner')
+    }
 };
 
 function oTurn() {
     //  = initState.o
     initState.turn = 'x'
+    initState.image = './images/crossbones.png'
+    initState.imageSize = '100px'
     initState.turnCount++
+    if (initState.turnCount > 5){
+        console.log('possible winner')
+    }
 
 }
 
@@ -76,10 +87,8 @@ col1.addEventListener('click', () =>{
     // col1.innerText = `${initState.turn}`
     col1.style.backgroundImage =`url(${initState.image})`
     col1.style.backgroundPosition = 'center'
-    col1.style.backgroundSize = '50px'
+    col1.style.backgroundSize = `${initState.imageSize}`
     col1.style.backgroundRepeat = 'no-repeat'
-    // col1.style.backgroundImage = `url(${initState.image})`
-    // col1.style.backgroundImage = 'url(./images/skull.png)'
     board[0] = `${initState.turn}`
     checkTurn()
     setTimeout(() => {
@@ -90,9 +99,12 @@ col1.addEventListener('click', () =>{
 
 col2.addEventListener('click', () =>{
     console.log('clicked')
-    col2.innerText = `${initState.turn}`
+    // col2.innerText = `${initState.turn}`
+    col2.style.backgroundImage =`url(${initState.image})`
+    col2.style.backgroundPosition = 'center'
+    col2.style.backgroundSize = `${initState.imageSize}`
+    col2.style.backgroundRepeat = 'no-repeat'
     board[1] = `${initState.turn}`
-    console.log(board[1])
         checkTurn()
         setTimeout(() => {
             col2.setAttribute('disabled', '')
@@ -101,7 +113,11 @@ col2.addEventListener('click', () =>{
 
 col3.addEventListener('click', () =>{
     console.log('clicked')
-    col3.innerText = `${initState.turn}`
+    // col3.innerText = `${initState.turn}`
+    col3.style.backgroundImage =`url(${initState.image})`
+    col3.style.backgroundPosition = 'center'
+    col3.style.backgroundSize = `${initState.imageSize}`
+    col3.style.backgroundRepeat = 'no-repeat'
     board[2] = `${initState.turn}`
         checkTurn()
         setTimeout(() => {
@@ -111,7 +127,11 @@ col3.addEventListener('click', () =>{
 
 col4.addEventListener('click', () =>{
     console.log('clicked')
-    col4.innerText = `${initState.turn}`
+    // col4.innerText = `${initState.turn}`
+    col4.style.backgroundImage =`url(${initState.image})`
+    col4.style.backgroundPosition = 'center'
+    col4.style.backgroundSize = `${initState.imageSize}`
+    col4.style.backgroundRepeat = 'no-repeat'
     board[3] = `${initState.turn}`
         checkTurn()
         setTimeout(() => {
@@ -121,7 +141,11 @@ col4.addEventListener('click', () =>{
 
 col5.addEventListener('click', () =>{
     console.log('clicked')
-    col5.innerText = `${initState.turn}`
+    // col5.innerText = `${initState.turn}`
+    col5.style.backgroundImage =`url(${initState.image})`
+    col5.style.backgroundPosition = 'center'
+    col5.style.backgroundSize = `${initState.imageSize}`
+    col5.style.backgroundRepeat = 'no-repeat'
     board[4] = `${initState.turn}`
         checkTurn()
         setTimeout(() => {
@@ -131,16 +155,25 @@ col5.addEventListener('click', () =>{
 
 col6.addEventListener('click', () =>{
     console.log('clicked')
-    col6.innerText = `${initState.turn}`
+    // col6.innerText = `${initState.turn}`
+    col6.style.backgroundImage =`url(${initState.image})`
+    col6.style.backgroundPosition = 'center'
+    col6.style.backgroundSize = `${initState.imageSize}`
+    col6.style.backgroundRepeat = 'no-repeat'
     board[5] = `${initState.turn}`
         checkTurn()
         setTimeout(() => {
             col6.setAttribute('disabled', '')
         }, "1000")
 })
+
 col7.addEventListener('click', () =>{
     console.log('clicked')
-    col7.innerText = `${initState.turn}`
+    // col7.innerText = `${initState.turn}`
+    col7.style.backgroundImage =`url(${initState.image})`
+    col7.style.backgroundPosition = 'center'
+    col7.style.backgroundSize = `${initState.imageSize}`
+    col7.style.backgroundRepeat = 'no-repeat'
     board[6] = `${initState.turn}`
         checkTurn()
         setTimeout(() => {
@@ -150,7 +183,11 @@ col7.addEventListener('click', () =>{
 
 col8.addEventListener('click', () =>{
     console.log('clicked')
-    col8.innerText = `${initState.turn}`
+    // col8.innerText = `${initState.turn}`
+    col8.style.backgroundImage =`url(${initState.image})`
+    col8.style.backgroundPosition = 'center'
+    col8.style.backgroundSize = `${initState.imageSize}`
+    col8.style.backgroundRepeat = 'no-repeat'
     board[7] = `${initState.turn}`
         checkTurn()
         setTimeout(() => {
@@ -160,7 +197,11 @@ col8.addEventListener('click', () =>{
 
 col9.addEventListener('click', () =>{
     console.log('clicked')
-    col9.innerText = `${initState.turn}`
+    // col9.innerText = `${initState.turn}`
+    col9.style.backgroundImage =`url(${initState.image})`
+    col9.style.backgroundPosition = 'center'
+    col9.style.backgroundSize = `${initState.imageSize}`
+    col9.style.backgroundRepeat = 'no-repeat'
     board[8] = `${initState.turn}`
         checkTurn()
         setTimeout(() => {
@@ -168,5 +209,4 @@ col9.addEventListener('click', () =>{
         }, "1000")
 })
 
-// createLayout(mainDiv, 'img', '', '', 'img')
-// img.setAttribute('src', './images/skull.png')
+console.log(board)
